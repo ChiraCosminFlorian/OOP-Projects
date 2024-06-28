5297a18456b2205668d8e0db67fcb2b2f7f2eba0
Chira Cosmin <146840512+ChiraCosminFlorian@users.noreply.github.com>
2024-06-28 15:58:12 +0300

    Add files via upload

---
+++ ChessLogic/Pieces/Queen.cs
@@ -0,0 +1,38 @@
+﻿namespace ChessLogic
+{
+    public class Queen : Piece
+    {
+        public override PieceType Type => PieceType.Queen;
+
+        public override Player Color { get; }
+
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.East,
+            Direction.North,
+            Direction.NorthEast,
+            Direction.NorthWest,
+            Direction.South,
+            Direction.SouthEast,
+            Direction.SouthWest,
+            Direction.West
+        };
+
+
+        public Queen(Player color)
+        {
+            Color = color;
+        }
+
+        public override Piece Copy()
+        {
+            Queen copy = new Queen(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositionInDirs(from, board, dirs).Select(to => new NormalMove(from, to));
+        }
+    }
+}
---
+++ ChessLogic/Player.cs
@@ -0,0 +1,28 @@
+﻿namespace ChessLogic
+{
+    public enum Player
+    {
+        None, //we can set the winning player to
+        //none in case of a draw
+        White,
+        Black
+    }
+
+    public static class PlayerExtensions
+    {
+        public static Player Opponent(this Player player)
+        {
+            switch (player)
+            {
+                case Player.White:
+                    return Player.Black;
+                case Player.Black:
+                    return Player.White;
+                default:
+                    return Player.None;
+
+            }
+        }
+    }
+
+}
